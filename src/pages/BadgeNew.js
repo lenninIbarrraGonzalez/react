d53dea7b53d725dfa4c728c58283f2e353210842
lenninIbarraGonzalez <soporte@allnexus.com.co>
2020-05-25 17:44:36 -0500

    Enlazando eventos

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -3,6 +3,7 @@ import NavBar from "../components/NavBar";
 import header from "../images/badge-header.svg";
 import Badge from "../components/Badges";
 import "./styles/BadgeNew.css";
+import BadgeForm from "../components/BadgeForm";
 
 class BadgeNew extends React.Component {
   render() {
@@ -14,7 +15,7 @@ class BadgeNew extends React.Component {
         </div>
         <div className="container">
           <div className="row">
-            <div className="col">
+            <div className="col-6">
               <Badge
                 firstName="Lennin"
                 lastName="Ibarra"
@@ -22,6 +23,9 @@ class BadgeNew extends React.Component {
                 jobTitle="Ingeniero de sistemas"
                 avatarUrl="https://scontent.fbog15-1.fna.fbcdn.net/v/t1.0-9/p960x960/98605375_1949342245196706_246653688272125952_o.jpg?_nc_cat=107&_nc_sid=85a577&_nc_ohc=fcw4umBTQVwAX-KTGgV&_nc_ht=scontent.fbog15-1.fna&_nc_tp=6&oh=b3bed8100dbb94b4dff034803684d173&oe=5EF1B0D0"
               />
+            </div>
+            <div className="col-6">
+              <BadgeForm />
             </div>
           </div>
         </div>
